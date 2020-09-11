9a94ec25b5e861236593b7c0b3bffb0fd93951a0
sungmin <sungmin69355@naver.com>
2020-09-11 15:21:37 +0900

    wsgi 수정

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -15,5 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
-path='/iteat/project/iteat/'
+path='/srv/iteat/project/'
 sys.path.insert(path)
